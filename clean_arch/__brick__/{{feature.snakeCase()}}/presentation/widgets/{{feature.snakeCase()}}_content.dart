64c88151f99b7398d8002dedc5d658c8af0083f5
Ahmed Nabil <ahmednab93@gmail.com>
2025-02-17 15:37:33 +0200

    refactor: add constructors with key parameter for screen and content

--- clean_arch/__brick__/{{feature.snakeCase()}}/presentation/widgets/{{feature.snakeCase()}}_content.dart
+++ clean_arch/__brick__/{{feature.snakeCase()}}/presentation/widgets/{{feature.snakeCase()}}_content.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class {{feature.pascalCase()}}Content extends StatelessWidget {
-  
+  const {{feature.pascalCase()}}Content({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Placeholder();
--- clean_arch/__brick__/{{feature.snakeCase()}}/presentation/{{feature.snakeCase()}}_screen.dart
+++ clean_arch/__brick__/{{feature.snakeCase()}}/presentation/{{feature.snakeCase()}}_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../index.dart';
 
-class {{feature.pascalCase()}}Page extends StatelessWidget {
+class {{feature.pascalCase()}}Screen extends StatelessWidget {
+const {{feature.pascalCase()}}Screen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
