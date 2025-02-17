8f610a44526de35e5a7ef8139af09860e17f9258
Ahmed Nabil <ahmednab93@gmail.com>
2025-02-17 15:21:45 +0200

    fix: correct the name of the UnimplementedError

--- clean_arch/__brick__/{{feature.snakeCase()}}/data/repos/{{feature.snakeCase()}}_repo_impl.dart
+++ clean_arch/__brick__/{{feature.snakeCase()}}/data/repos/{{feature.snakeCase()}}_repo_impl.dart
@@ -5,6 +5,6 @@ class {{feature.pascalCase()}}RepoImpl implements {{feature.pascalCase()}}Repo {
   @override
   Future<Either<Failure, {{feature.pascalCase()}}Model>> get{{feature.pascalCase()}}() {
     ///TODO: implement
-    throw NotImplementedException();
+    throw UnimplementedError();
   }
 }
