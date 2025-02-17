16eea7ed92f55440787cd4a969976e67be5f503d
Ahmed Nabil <ahmednab93@gmail.com>
2025-02-17 15:38:11 +0200

    refactor: remove unncessary provider import

--- clean_arch/__brick__/{{feature.snakeCase()}}/presentation/widgets/{{feature.snakeCase()}}_content.dart
+++ clean_arch/__brick__/{{feature.snakeCase()}}/presentation/widgets/{{feature.snakeCase()}}_content.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class {{feature.pascalCase()}}Content extends StatelessWidget {
   const {{feature.pascalCase()}}Content({super.key});
