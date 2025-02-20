c8dbc5b2d0e913d8380fccf40e58c782e90e7bde
Ahmed Nabil <ahmednab93@gmail.com>
2025-02-20 11:07:56 +0200

    ✨ feat: introduce base state class

--- clean_arch/__brick__/features/{{feature.snakeCase()}}/presentation/provider/{{feature.snakeCase()}}_state.dart
+++ clean_arch/__brick__/features/{{feature.snakeCase()}}/presentation/provider/{{feature.snakeCase()}}_state.dart
@@ -1,8 +1,9 @@
 import '../../index.dart';
 {{#create_bases}}import '../../../../core/{{failure.snakeCase()}}.dart';{{/create_bases}}
 {{#create_bases}}import '../../../../core/{{provider_status.snakeCase()}}.dart';{{/create_bases}}
+{{#create_bases}}import '../../../../core/{{base_state.snakeCase()}}.dart';{{/create_bases}}
 
-class {{feature.pascalCase()}}State extends BaseState {
+final class {{feature.pascalCase()}}State extends {{base_state.pascalCase()}} {
 
   const {{feature.pascalCase()}}State({
     super.status = {{provider_status.pascalCase()}}.initial,
