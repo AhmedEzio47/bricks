843161058bfe3517c70227e8a67e2cfabbf15154
Ahmed Nabil <ahmednab93@gmail.com>
2025-02-24 13:26:16 +0200

    ✨ feat(clean_arch):  add a variable to generate entity properties

--- clean_arch/__brick__/features/{{feature.snakeCase()}}/domain/entities/{{feature.snakeCase()}}_entity.dart
+++ clean_arch/__brick__/features/{{feature.snakeCase()}}/domain/entities/{{feature.snakeCase()}}_entity.dart
@@ -1,3 +1,9 @@
 class {{feature.pascalCase()}}Entity {
-  
+    const {{feature.pascalCase()}}Entity ({
+        {{#properties}} this.{{name.camelCase()}},
+        {{/properties}}
+    });
+
+    {{#properties}}final {{{type}}}? {{name.camelCase()}};
+    {{/properties}}
 }
